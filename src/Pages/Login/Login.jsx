c1f54da56265afbe350b42707345429b997ae13c
nasirullah <nasrullah23a@gmail.com>
2022-02-10 08:47:30 +0600

    Login Complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,95 +1,104 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import localforage from "localforage";
 import avatar from "./user.svg";
 import "./login.css";
-
-class Login extends Component {
-	constructor(props) {
-		super(props)
-		this.wrap = React.createRef();
-	}
-	state = {
-		email: '',
-		password: '',
-		remember: false,
-	}
-	handleInp = (e) => {
-		if (e.target.name === 'email') {
-			this.setState({email: e.target.value});
-		}
-		if (e.target.name === 'password') {
-			this.setState({password: e.target.value});
-		}
-		if (e.target.name === 'remember') {
-			this.setState({remember: e.target.checked});
-		}
-	}
-	login = () => {
-		// checkboxEffect
-		let wrap = this.wrap.current;
-		wrap.style.animationName = 'checkboxEffect';
-		wrap.addEventListener('animationend', function() {
-			wrap.style.animationName = '';
+axios.defaults.baseURL = 'http://localhost:8000';
+// axios.defaults.headers.common['Authorization'] = '';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+localforage.config({
+    driver      : localforage.LOCALSTORAGE, // Force WebSQL; same as using setDriver()
+    name        : 'myApp',
+    version     : 1.0,
+    size        : 4980736, // Size of database, in bytes. WebSQL-only for now.
+    storeName   : 'keyvaluepairs', // Should be alphanumeric, with underscores.
+    description : 'some description'
+});
+const Login = ({ setToken })  => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [remember, setRemember] = useState(false);
+	const wrap = useRef();
+	const navigate = useNavigate();
+	useEffect(() => {
+		localforage.getItem('token', function (err, value) {
+			if (value) {
+				setToken(value);
+				navigate('/');
+			}
 		});
+	}, [navigate, setToken]);
+	const login = () => {
+		// checkboxEffect
+		// wrap.current.style.animationName = 'checkboxEffect';
+		// wrap.current.addEventListener('animationend', function() {
+		// 	wrap.style.animationName = '';
+		// });
 		// axios
-		axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie')
-			.then(response => {
-				console.log(response);
-				// axios.post('http://127.0.0.1:8000/api/login', {
-				// 	email: this.email,
-				// 	password: this.password
-				// }).then(response => {
-				// 	console.log(response)
-				// })
-			});
+		axios.post('/api/login', {
+			email: email,
+			password: password
+		}).then(response => {
+			if (response.data) {
+				let data = response.data;
+				localforage.setItem('token', data.token).then(function () {
+					return localforage.getItem('token');
+				}).then(function (value) {
+					setToken(value);
+					navigate('/');
+				}).catch(function (err) {
+					console.log(err);
+				});
+			}
+		});
 	}
-	render() {
-		const { email, password, remember } = this.state;
-		return (
-			<div className="wrap" ref={this.wrap}>
-				<div className="frm">
-					<div className="had">
-						<img src={avatar} alt="" />
-					</div>
-					<h2 style={{ textAlign: "center" }}>Admin Login</h2>
-					<input
-						className="fld"
-						type="email"
-						name="email"
-						placeholder="Email"
-						value={email}
-						onChange={this.handleInp}
-						required
-					/>
-					<input
-						style={{ marginBottom: "20px" }}
-						className="fld"
-						type="password"
-						name="password"
-						placeholder="Password"
-						value={password}
-						onChange={this.handleInp}
-						required
-					/>
-					<div style={{ marginBottom: "20px" }}>
-						<input
-						className="remember"
-						type="checkbox"
-						id="remember"
-						value={remember}
-						onChange={this.handleInp}
-						name="remember"
-						/>
-						<label htmlFor="remember" className="fre">
-						<span className="fre_check"></span>
-						Accept all things
-						</label>
-					</div>
-					<button className="lgn" type="submit" onClick={this.login}>Login</button>
+	
+	return (
+		<div className="wrap" ref={wrap}>
+			<div className="frm">
+				<div className="had">
+					<img src={avatar} alt="" />
 				</div>
+				<h2 style={{ textAlign: "center" }}>Admin Login</h2>
+				<input
+					className="fld"
+					type="email"
+					name="email"
+					placeholder="Email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					required
+				/>
+				<input
+					style={{ marginBottom: "20px" }}
+					className="fld"
+					type="password"
+					name="password"
+					placeholder="Password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					required
+				/>
+				<div style={{ marginBottom: "20px" }}>
+					<input
+					className="remember"
+					type="checkbox"
+					id="remember"
+					value={remember}
+					onChange={(e) => setRemember(e.target.checked)}
+					name="remember"
+					/>
+					<label htmlFor="remember" className="fre">
+					<span className="fre_check"></span>
+					Accept all things
+					</label>
+				</div>
+				<button className="lgn" type="submit" onClick={() => login()}>Login</button>
 			</div>
-		);
-	}
+		</div>
+	);
+
 }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
-import Login from './Pages/Login/Login';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
